cfec8314d4346115c2772cedf5ba116eb06b7530
Saurav  Suman <60661830+sks-saurav@users.noreply.github.com>
2026-05-31 19:51:18 +0530

    Time: 3 ms (26.4%), Space: 19.3 MB (40.39%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,25 +1,47 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        
-        @cache
-        def helper(idx):
-            if idx == len(s):
-                return 1
+        n = len(s)
+        dp = [0] * (n+1)
+        dp[n] = 1
 
+        for idx in range(n-1, -1, -1):
             dig1 = ord(s[idx]) - ord('0')
-
             ways = 0
             if dig1 != 0:
-                ways = helper(idx+1)
+                ways = dp[idx+1]
 
             if idx+1 < len(s):
                 dig2 = ord(s[idx+1]) - ord('0')
 
                 if dig1 == 1:
-                    ways += helper(idx+2)
+                    ways += dp[idx+2]
                 elif dig1 == 2 and dig2 <= 6:
-                    ways += helper(idx+2)
+                    ways += dp[idx+2]
+            
+            dp[idx] = ways
 
-            return ways
+        return dp[0]
 
-        return helper(0)
+        
+        # @cache
+        # def helper(idx):
+        #     if idx == len(s):
+        #         return 1
+
+        #     dig1 = ord(s[idx]) - ord('0')
+
+        #     ways = 0
+        #     if dig1 != 0:
+        #         ways = helper(idx+1)
+
+        #     if idx+1 < len(s):
+        #         dig2 = ord(s[idx+1]) - ord('0')
+
+        #         if dig1 == 1:
+        #             ways += helper(idx+2)
+        #         elif dig1 == 2 and dig2 <= 6:
+        #             ways += helper(idx+2)
+
+        #     return ways
+
+        # return helper(0)
