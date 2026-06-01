ffd8e1c197ed7704a2d1699623e8f66c57d75fb4
Saurav  Suman <60661830+sks-saurav@users.noreply.github.com>
2026-06-01 23:44:03 +0530

    Time: 164 ms (86.11%), Space: 19.9 MB (86.46%) - LeetHub

--- 0639-decode-ways-ii/0639-decode-ways-ii.py
+++ 0639-decode-ways-ii/0639-decode-ways-ii.py
@@ -1,52 +1,65 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         MOD = int(1e9) + 7
-
-        @cache
-        def helper(idx):
-            if idx >= len(s):
-                return 1
-
-            ways = 0
-            ch = s[idx]
+        
+        # If the string starts with '0', it's immediately invalid
+        if not s or s[0] == '0':
+            return 0
             
-            if ch == '0':
-                return 0
-
-            if ch == '*':
-                ways = (ways + (9 * helper(idx+1))) % MOD
-                if idx + 1 < len(s):
-                    n_ch = s[idx+1]
-                    if n_ch == '*':
-                        # "**" can be "11"-"19" (9) or "21"-"26" (6) -> 15 ways
-                        ways = (ways + (15 * helper(idx+2))) % MOD
-                    elif n_ch <= '6':
-                        # "*0" to "*6" -> '*' can be '1' or '2' -> 2 ways
-                        ways = (ways + (2 * helper(idx+2))) % MOD
-                    else:
-                        # "*7" to "*9" -> '*' can only be '1' -> 1 way
-                        ways = (ways + helper(idx+2)) % MOD
-
-            else:
-                ways = (ways + helper(idx+1)) % MOD
-
-                if idx+1 < len(s):
-                    n_ch = s[idx+1]
-
-                    if ch == '1':
-                        if n_ch == '*':
-                            ways = (ways + (9 * helper(idx+2))) % MOD
-                        else:
-                            ways = (ways + helper(idx+2)) % MOD
-
-                    elif ch == '2':
-                        if n_ch == '*':
-                            ways = (ways + (6 * helper(idx+2))) % MOD
-                        elif (ord(n_ch) - ord('0')) <= 6:
-                            ways = (ways + helper(idx+2)) % MOD
-
-            return ways
-
-
-        return helper(0)
+        # Base cases for our DP
+        # prev2 represents dp[i-2] (empty string prefix)
+        prev2 = 1 
+        
+        # prev1 represents dp[i-1] (first character prefix)
+        prev1 = 9 if s[0] == '*' else 1
+        
+        for i in range(1, len(s)):
+            curr = 0
+            char = s[i]
+            prev_char = s[i-1]
             
+            # ----------------------------------------------------
+            # 1-digit decoding (depends on prev1)
+            # ----------------------------------------------------
+            if char == '*':
+                curr = (curr + 9 * prev1) % MOD
+            elif char != '0':
+                curr = (curr + prev1) % MOD
+                
+            # ----------------------------------------------------
+            # 2-digit decoding (depends on prev2)
+            # ----------------------------------------------------
+            if prev_char == '*':
+                if char == '*':
+                    # "**" -> "11"-"19" (9 ways) + "21"-"26" (6 ways) = 15 ways
+                    curr = (curr + 15 * prev2) % MOD
+                elif char <= '6':
+                    # "*0"-"*6" -> '*' can be '1' or '2' -> 2 ways
+                    curr = (curr + 2 * prev2) % MOD
+                else:
+                    # "*7"-"*9" -> '*' can only be '1' -> 1 way
+                    curr = (curr + prev2) % MOD
+                    
+            elif prev_char == '1':
+                if char == '*':
+                    # "1*" -> "11"-"19" -> 9 ways
+                    curr = (curr + 9 * prev2) % MOD
+                else:
+                    # "10"-"19" -> 1 way
+                    curr = (curr + prev2) % MOD
+                    
+            elif prev_char == '2':
+                if char == '*':
+                    # "2*" -> "21"-"26" -> 6 ways
+                    curr = (curr + 6 * prev2) % MOD
+                elif char <= '6':
+                    # "20"-"26" -> 1 way
+                    curr = (curr + prev2) % MOD
+                    
+            # ----------------------------------------------------
+            # Shift states forward for the next iteration
+            # ----------------------------------------------------
+            prev2 = prev1
+            prev1 = curr
+            
+        return prev1
