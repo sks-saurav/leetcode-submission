0b062c684b73f868c2977f0b8e4e8c669a35caed
Saurav  Suman <60661830+sks-saurav@users.noreply.github.com>
2026-06-02 10:15:00 +0530

    Time: 0 ms (100%), Space: 19.6 MB (64.27%) - LeetHub

--- 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.py
+++ 1105-filling-bookcase-shelves/1105-filling-bookcase-shelves.py
@@ -2,27 +2,49 @@ class Solution:
     def minHeightShelves(self, books: List[List[int]], shelfWidth: int) -> int:
         n = len(books)
 
-        @cache
-        def dp(idx):
-            if idx >= n:
-                return 0
-
-            width = shelfWidth
+        # bottoms up        
+        # dp[i] stores the minimum height to place the first i books.
+        dp = [float('inf')] * (n + 1)
+        dp[0] = 0
+        
+        for i in range(1, n + 1):
+            curr_width = 0
             curr_height = 0
-            ans = int(1e9)
+            
+            # Look backwards from the current book 'i' to see how many previous 
+            # books we can group together on the same shelf.
+            for j in range(i, 0, -1):
+                curr_width += books[j-1][0]
+                
+                if curr_width > shelfWidth:
+                    break
+                
+                curr_height = max(curr_height, books[j-1][1])
+                
+                dp[i] = min(dp[i], dp[j-1] + curr_height)
+                
+        return dp[n]
 
-            while idx < n and width - books[idx][0] >= 0:
-                width -= books[idx][0]
-                curr_height = max(curr_height, books[idx][1])
 
-                ans = min(ans, dp(idx+1) + curr_height)
-                idx += 1
+        # memoization
+        # @cache
+        # def dp(idx):
+        #     if idx >= n:
+        #         return 0
 
-            return ans
+        #     width = shelfWidth
+        #     curr_height = 0
+        #     ans = int(1e9)
 
+        #     while idx < n and width - books[idx][0] >= 0:
+        #         width -= books[idx][0]
+        #         curr_height = max(curr_height, books[idx][1])
 
-        return dp(0)
+        #         ans = min(ans, dp(idx+1) + curr_height)
+        #         idx += 1
+        #     return ans
 
+        # return dp(0)
 
 
 
