c0d4782d8a667ecd07cfec3a273d28c2011fcbe4
Saurav  Suman <60661830+sks-saurav@users.noreply.github.com>
2026-06-16 10:10:45 +0530

    Time: 35 ms (86.08%), Space: 21 MB (13.92%) - LeetHub

--- 1278-palindrome-partitioning-iii/1278-palindrome-partitioning-iii.py
+++ 1278-palindrome-partitioning-iii/1278-palindrome-partitioning-iii.py
@@ -1,5 +1,6 @@
 class Solution:
     def palindromePartition(self, s: str, k: int) -> int:
+        @cache
         def count_change(st, end):
             count = 0
             while st < end:
@@ -9,6 +10,7 @@ class Solution:
                 end -= 1
             return count
 
+        # Memoization DP
         @cache
         def helper(st, rk):
             if rk == 1:
@@ -22,3 +24,25 @@ class Solution:
             return ans
 
         return helper(0, k)
+
+# ## Bottom Up DP
+# class Solution:
+#     def palindromePartition(self, s: str, k: int) -> int:
+#         n = len(s)
+#         cost = [[0] * n for _ in range(n)]
+#         for length in range(2, n + 1):
+#             for i in range(n - length + 1):
+#                 j = i + length - 1
+#                 cost[i][j] = cost[i+1][j-1] + (1 if s[i] != s[j] else 0)
+
+#         dp = [[float('inf')] * (k + 1) for _ in range(n)]
+        
+#         for i in range(n):
+#             dp[i][1] = cost[0][i]
+            
+#         for j in range(2, k + 1):          # For each number of partitions from 2 to k
+#             for i in range(j - 1, n):      # End index of the current prefix
+#                 for m in range(j - 2, i):
+#                     dp[i][j] = min(dp[i][j], dp[m][j-1] + cost[m+1][i])
+                    
+#         return dp[n-1][k]
