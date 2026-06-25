e6c6fe27055bcf386fa87d9f8d3ae44fc453120b
Saurav  Suman <60661830+sks-saurav@users.noreply.github.com>
2026-06-25 07:54:45 +0530

    Time: 11 ms (70.07%), Space: 22.5 MB (54.11%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.py
+++ 0997-find-the-town-judge/0997-find-the-town-judge.py
@@ -1,17 +1,16 @@
 class Solution:
-    def findJudge(self, n: int, trust: List[List[int]]) -> int:
-        if n == 1:
-            return 1 if len(trust) == 0 else -1
-            
-        trust_map = defaultdict(int)
-        trust_rev_map = defaultdict(int)
+    def findJudge(self, N: int, trust: List[List[int]]) -> int:
+
+        if len(trust) < N - 1:
+            return -1
+
+        trust_scores = [0] * (N + 1)
+
         for a, b in trust:
-            trust_map[b] += 1
-            trust_rev_map[a] += 1
-
-
-        for k in trust_map:
-            if trust_map[k] >= n-1 and trust_rev_map[k] == 0:
-                return k
-
+            trust_scores[a] -= 1
+            trust_scores[b] += 1
+        
+        for i, score in enumerate(trust_scores[1:], 1):
+            if score == N - 1:
+                return i
         return -1
