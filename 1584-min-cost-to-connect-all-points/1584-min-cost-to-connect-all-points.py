6b9c0e38bc45f11e72d57c38590cfde7ef635585
Saurav  Suman <60661830+sks-saurav@users.noreply.github.com>
2026-06-27 21:07:30 +0530

    Time: 3145 ms (12.7%), Space: 92.3 MB (52.49%) - LeetHub

---
+++ 1584-min-cost-to-connect-all-points/1584-min-cost-to-connect-all-points.py
@@ -0,0 +1,37 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        n = len(points)
+        parent = [i for i in range(n)]
+
+        def get_parent(u):
+            if parent[u] != u:
+                parent[u] = get_parent(parent[u])
+            return parent[u]
+
+        def union(u, v):
+            pu = get_parent(u)
+            pv = get_parent(v)
+
+            if pu != pv:
+                parent[pu] = pv
+                return True
+
+            return False
+
+        heap = []
+        for i in range(n):
+            for j in range(i+1, n):
+                dist = abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1])
+                heappush(heap, (dist, i, j))
+
+        ans = 0
+        edge_count = 0
+        while heap:
+            dist, u, v = heappop(heap)
+            if union(u, v):
+                edge_count += 1
+                ans += dist
+
+        if edge_count == n-1:
+            return ans
+        return -1
