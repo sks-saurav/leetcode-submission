27e4fcb2a5a97e91c1207b116b64f10fd51e2b6b
Saurav  Suman <60661830+sks-saurav@users.noreply.github.com>
2026-06-28 11:49:16 +0530

    Time: 502 ms (48.39%), Space: 35.9 MB (29.03%) - LeetHub

--- 2077-paths-in-maze-that-lead-to-same-room/2077-paths-in-maze-that-lead-to-same-room.py
+++ 2077-paths-in-maze-that-lead-to-same-room/2077-paths-in-maze-that-lead-to-same-room.py
@@ -1,23 +1,14 @@
 class Solution:
     def numberOfPaths(self, n: int, corridors: List[List[int]]) -> int:
         adj = defaultdict(set)
-        for a, b in corridors:
-            adj[a].add(b)
-            adj[b].add(a)
+        
+        for u, v in corridors:
+            adj[u].add(v)
+            adj[v].add(u)
 
-        def count_three_len_cycle(node):
-            res = 0
-            nb = list(adj[node])
-            for i in range(len(nb)):
-                for j in range(i+1, len(nb)):
-                    a, b = nb[i], nb[j]
-                    if a in adj[b]:
-                        res += 1
-
-            return res
+        ans = 0
+        for u, v in corridors:
+            # Common neighbors of u and v form a cycle of length 3
+            ans += len(adj[u] & adj[v])
             
-
-        final_ans = 0
-        for node in range(1, n+1):
-            final_ans += count_three_len_cycle(node)
-        return final_ans//3
+        return ans // 3
