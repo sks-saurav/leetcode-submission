5fd0c760517baeac4468e9a69b65fd47bb64fb4c
Saurav  Suman <60661830+sks-saurav@users.noreply.github.com>
2026-06-28 11:53:26 +0530

    Time: 209 ms (83.87%), Space: 30.4 MB (90.32%) - LeetHub

--- 2077-paths-in-maze-that-lead-to-same-room/2077-paths-in-maze-that-lead-to-same-room.py
+++ 2077-paths-in-maze-that-lead-to-same-room/2077-paths-in-maze-that-lead-to-same-room.py
@@ -1,14 +1,22 @@
+from typing import List
+
 class Solution:
     def numberOfPaths(self, n: int, corridors: List[List[int]]) -> int:
-        adj = defaultdict(set)
-        
+        degree = [0] * (n + 1)
         for u, v in corridors:
-            adj[u].add(v)
-            adj[v].add(u)
-
-        ans = 0
-        for u, v in corridors:
-            # Common neighbors of u and v form a cycle of length 3
-            ans += len(adj[u] & adj[v])
+            degree[u] += 1
+            degree[v] += 1
             
-        return ans // 3
+        adj = defaultdict(set)
+        for u, v in corridors:
+            if degree[u] < degree[v] or (degree[u] == degree[v] and u < v):
+                adj[u].add(v)
+            else:
+                adj[v].add(u)
+                
+        ans = 0
+        for u in range(1, n + 1):
+            for v in adj[u]:
+                ans += len(adj[u] & adj[v])
+                
+        return ans
