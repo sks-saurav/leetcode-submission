aa6ff36a6c640bdb30c9b6332b0d953b87601071
Saurav  Suman <60661830+sks-saurav@users.noreply.github.com>
2026-06-30 08:17:39 +0530

    Time: 58 ms (93.97%), Space: 25.2 MB (32.76%) - LeetHub

--- 0510-inorder-successor-in-bst-ii/0510-inorder-successor-in-bst-ii.py
+++ 0510-inorder-successor-in-bst-ii/0510-inorder-successor-in-bst-ii.py
@@ -9,16 +9,50 @@ class Node:
         self.parent = None
 """
 
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.parent = None
+"""
+
 class Solution:
-    def inorderSuccessor(self, node: 'Node') -> 'Node':
-        # the successor is somewhere lower in the right subtree
+    def inorderSuccessor(self, node: 'Node') -> 'Optional[Node]':
+        # if node has right child -> successor present in rhs
+        # else successor is above in parent
+        successor = None
+        target_val = node.val
+
+        def get_successor_above(node):
+            nonlocal successor
+
+            while node:
+                if node.val > target_val:
+                    successor = node
+                    break
+                
+                node = node.parent
+
+
+        def get_successor_below(node):
+            nonlocal successor
+
+            if not node:
+                return
+
+            if node.val > target_val:
+                successor = node
+                get_successor_below(node.left)
+            else:
+                get_successor_below(node.right)
+
+
         if node.right:
-            node = node.right
-            while node.left:
-                node = node.left
-            return node
-        
-        # the successor is somewhere upper in the tree
-        while node.parent and node == node.parent.right:
-            node = node.parent
-        return node.parent
+            get_successor_below(node.right)
+        else:
+            get_successor_above(node.parent)
+
+        return  successor
