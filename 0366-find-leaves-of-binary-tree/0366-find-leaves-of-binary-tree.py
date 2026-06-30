29ee38eb1049d2eef7f4e0b7b412f6c4be9f4241
Saurav  Suman <60661830+sks-saurav@users.noreply.github.com>
2026-06-30 10:19:26 +0530

    Time: 1 ms (6.58%), Space: 19.5 MB (15.64%) - LeetHub

--- 0366-find-leaves-of-binary-tree/0366-find-leaves-of-binary-tree.py
+++ 0366-find-leaves-of-binary-tree/0366-find-leaves-of-binary-tree.py
@@ -33,3 +33,48 @@ class Solution:
 
         return ans
 
+
+# BFS indegree solution
+# class Solution:
+#     def findLeaves(self, root: Optional[TreeNode]) -> List[List[int]]:
+#         parent = {}
+#         parent[root] = None
+
+#         que = deque()
+#         indeg = defaultdict(int)
+
+#         def traverse(node, node_p):
+#             if node is None:
+#                 return
+
+#             parent[node] = node_p
+#             if node.left is None and node.right is None:
+#                 que.append(node)
+
+#             if node.left:
+#                 indeg[node] += 1
+#                 traverse(node.left, node)
+
+#             if node.right:
+#                 indeg[node] += 1
+#                 traverse(node.right, node)
+
+#         traverse(root, None)
+#         ans = []
+#         while que:
+#             ll = len(que)
+#             tans = []
+
+#             for _ in range(ll):
+#                 curr = que.popleft()
+#                 tans.append(curr.val)
+#                 p_curr = parent[curr]
+
+#                 if p_curr:
+#                     indeg[p_curr] -= 1
+#                     if indeg[p_curr] == 0:
+#                         que.append(p_curr)
+
+#             ans.append(tans)
+
+#         return ans
