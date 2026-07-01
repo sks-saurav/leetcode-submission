5bc1a7369a208d60939b4f737aa93773b13368b4
Saurav  Suman <60661830+sks-saurav@users.noreply.github.com>
2026-07-01 08:27:16 +0530

    Time: 12 ms (32.6%), Space: 23.5 MB (13.92%) - LeetHub

---
+++ 0298-binary-tree-longest-consecutive-sequence/0298-binary-tree-longest-consecutive-sequence.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def longestConsecutive(self, root: Optional[TreeNode]) -> int:
+        self.ans = 0
+
+        def path(node):
+            if node is None:
+                return 0
+                
+            curr_len = 1
+            pl = path(node.left)
+            pr = path(node.right)
+
+            if node.left and node.left.val == node.val + 1:
+                curr_len = max(curr_len, 1 + pl)
+            if node.right and node.right.val == node.val + 1:
+                curr_len = max(curr_len, 1 + pr)
+            self.ans = max(self.ans, curr_len)
+
+            return curr_len
+
+        path(root)
+        return self.ans
