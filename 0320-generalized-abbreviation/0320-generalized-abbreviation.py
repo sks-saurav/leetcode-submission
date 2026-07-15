bac84d347890ad51b58f574eb4c974a6db285a28
Saurav  Suman <60661830+sks-saurav@users.noreply.github.com>
2026-07-15 08:07:16 +0530

    Time: 24 ms (68.63%), Space: 25.1 MB (14.71%) - LeetHub

--- 0320-generalized-abbreviation/0320-generalized-abbreviation.py
+++ 0320-generalized-abbreviation/0320-generalized-abbreviation.py
@@ -2,21 +2,17 @@ class Solution:
     def generateAbbreviations(self, word: str) -> List[str]:
         ans = []
 
-        def find_abv(idx, arr, last_abv):
+        def find_abv(idx, res, last_abv):
             if idx >= len(word):
-                ans.append(''.join(arr))
+                ans.append(res)
                 return
 
-            arr.append(word[idx])
-            find_abv(idx+1, arr, False)
-            arr.pop()
+            find_abv(idx+1, res+word[idx], False)
 
             if not last_abv:
                 for j in range(idx+1, len(word)+1):
-                    arr.append(str(j-idx))
-                    find_abv(j, arr, True)
-                    arr.pop()
+                    find_abv(j, res+str(j-idx), True)
 
-        find_abv(0, [], False)
+        find_abv(0, "", False)
         return ans
 
