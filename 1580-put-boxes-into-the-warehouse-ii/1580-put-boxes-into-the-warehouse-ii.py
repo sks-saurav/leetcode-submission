db005c60d18da1ecd6498757060c31060eadcaee
Saurav  Suman <60661830+sks-saurav@users.noreply.github.com>
2026-08-03 08:58:53 +0530

    Time: 59 ms (64.39%), Space: 39.3 MB (12.2%) - LeetHub

--- 1580-put-boxes-into-the-warehouse-ii/1580-put-boxes-into-the-warehouse-ii.py
+++ 1580-put-boxes-into-the-warehouse-ii/1580-put-boxes-into-the-warehouse-ii.py
@@ -1,32 +1,21 @@
 class Solution:
     def maxBoxesInWarehouse(self, boxes: List[int], warehouse: List[int]) -> int:
         n = len(warehouse)
-        minHeight = float("inf")
-        effectiveHeights = [0] * n
-
-        for i in range(n):
-            minHeight = min(minHeight, warehouse[i])
-            effectiveHeights[i] = minHeight
-
-        minHeight = float("inf")
-        for i in range(n - 1, -1, -1):
-            minHeight = min(minHeight, warehouse[i])
-            effectiveHeights[i] = max(effectiveHeights[i], minHeight)
-
         ans = 0
-        i, wa, wb = 0, 0, len(effectiveHeights)-1
+        i, wa, wb = 0, 0, len(warehouse)-1
+        wl, wr = float('inf'), float('inf')
         boxes.sort(reverse=True)
 
         while i < len(boxes) and wa <= wb:
-            if boxes[i] <= effectiveHeights[wa]:
+            if boxes[i] <= min(wl, warehouse[wa]):
+                wl =  min(wl, warehouse[wa])
                 wa += 1
                 ans += 1
-                i += 1
-            elif boxes[i] <= effectiveHeights[wb]:
+            elif boxes[i] <=  min(wr, warehouse[wb]):
+                wr =  min(wr, warehouse[wb])
                 wb -= 1
                 ans += 1
-                i += 1
-            else:
-                i += 1
+            
+            i += 1
 
         return ans
