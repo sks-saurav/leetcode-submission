301184c5ef36d2625a38a0614042d93ada98b9f5
Saurav  Suman <60661830+sks-saurav@users.noreply.github.com>
2026-09-22 10:17:27 +0530

    Time: 4 ms (41.24%), Space: 19.7 MB (39.82%) - LeetHub

---
+++ 0871-minimum-number-of-refueling-stops/0871-minimum-number-of-refueling-stops.py
@@ -0,0 +1,23 @@
+import heapq
+from typing import List
+
+class Solution:
+    def minRefuelStops(self, target: int, startFuel: int, stations: List[List[int]]) -> int:
+        max_heap = []
+        max_reach = startFuel
+        stops = 0
+
+        # Treat the target as a terminal station with 0 fuel
+        for pos, fuel in stations + [[target, 0]]:
+            # If we cannot reach the current station/target, refuel from the best past stations
+            while max_reach < pos and max_heap:
+                max_reach += -heapq.heappop(max_heap)
+                stops += 1
+
+            # If we still can't reach this position, reaching target is impossible
+            if max_reach < pos:
+                return -1
+
+            heapq.heappush(max_heap, -fuel)
+
+        return stops
