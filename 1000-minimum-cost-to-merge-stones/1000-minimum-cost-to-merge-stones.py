7d539a17c639dcdcb05b7865e89fa51fe1458de2
Saurav  Suman <60661830+sks-saurav@users.noreply.github.com>
2026-09-25 09:36:29 +0530

    Time: 7 ms (77.52%), Space: 19.3 MB (82.55%) - LeetHub

--- 1000-minimum-cost-to-merge-stones/1000-minimum-cost-to-merge-stones.py
+++ 1000-minimum-cost-to-merge-stones/1000-minimum-cost-to-merge-stones.py
@@ -1,81 +1,108 @@
 class Solution:
     def mergeStones(self, stones: List[int], k: int) -> int:
         n = len(stones)
-        if (n - 1) % (k - 1) != 0:
-            return -1
-            
-        prefix = [0] * (n + 1)
+        if (n-1)%(k-1) != 0: return -1
+
+        psum = [0] * (n + 1)
         for i in range(n):
-            prefix[i + 1] = prefix[i] + stones[i]
+            psum[i + 1] = psum[i] + stones[i]
             
-        def range_sum(i: int, j: int) -> int:
-            return prefix[j + 1] - prefix[i]
-            
-        memo = {}
+        def rsum(st, end):
+            return psum[end+1] - psum[st]
+
+        dp = [[0]*n for _ in range(n)]
+
+        for l in range(k, n+1):
+            for st in range(n-l+1):
+                end = st+l-1
+
+                dp[st][end] = float('inf')
+
+                for mid in range(st, end, k-1):
+                    dp[st][end] = min(dp[st][end], dp[st][mid] + dp[mid+1][end])
+                
+                if (end-st)%(k-1) == 0:
+                    dp[st][end] += rsum(st, end)
+
+        return dp[0][n-1]
+
         
-        def solve(i: int, j: int) -> int:
-            if i == j:
-                return 0
-                
-            if (i, j) in memo:
-                return memo[(i, j)]
-                
-            min_cost = float('inf')
+# class Solution:
+#     def mergeStones(self, stones: List[int], k: int) -> int:
+#         n = len(stones)
+#         if (n - 1) % (k - 1) != 0:
+#             return -1
             
-            # We step by (k - 1) to ensure the left part stones[i...mid] 
-            for mid in range(i, j, k - 1):
-                cost = solve(i, mid) + solve(mid + 1, j)
-                if cost < min_cost:
-                    min_cost = cost
+#         prefix = [0] * (n + 1)
+#         for i in range(n):
+#             prefix[i + 1] = prefix[i] + stones[i]
+            
+#         def range_sum(i: int, j: int) -> int:
+#             return prefix[j + 1] - prefix[i]
+            
+#         memo = {}
+        
+#         def solve(i: int, j: int) -> int:
+#             if i == j:
+#                 return 0
+                
+#             if (i, j) in memo:
+#                 return memo[(i, j)]
+                
+#             min_cost = float('inf')
+            
+#             # We step by (k - 1) to ensure the left part stones[i...mid] 
+#             for mid in range(i, j, k - 1):
+#                 cost = solve(i, mid) + solve(mid + 1, j)
+#                 if cost < min_cost:
+#                     min_cost = cost
                     
-            # If the entire subarray stones[i...j] is ready to be merged 
-            # into exactly 1 pile, add the cost of this final merge.
-            if (j - i) % (k - 1) == 0:
-                min_cost += range_sum(i, j)
+#             # If the entire subarray stones[i...j] is ready to be merged 
+#             # into exactly 1 pile, add the cost of this final merge.
+#             if (j - i) % (k - 1) == 0:
+#                 min_cost += range_sum(i, j)
                 
-            memo[(i, j)] = min_cost
-            return min_cost
+#             memo[(i, j)] = min_cost
+#             return min_cost
             
-        return solve(0, n - 1)
+#         return solve(0, n - 1)
 
+'''
+The first thing we need to check can we merge all these piles into 1 pile ?
+we can check this : if((n-1)%(k-1) == 0) then only we can do merging.
 
+let's see how we can arrive at this formula
 
-        '''
-        The first thing we need to check can we merge all these piles into 1 pile ?
-        we can check this : if((n-1)%(k-1) == 0) then only we can do merging.
+if there are n piles given and k piles are to be merged. Then,
+After the first merge : the length of the array will be n - (k-1).
+After the second merge : the length of the array will be n - (k-1) - (k-1).
+After the third merge : the length of the array will be n - (k-1) - (k-1) - (k-1)
+and so on...
 
-        let's see how we can arrive at this formula
+For eg:
+[0 1 2 3 4] -> indexing
+[3,5,1,2,6] -> array elements
+length is 5
 
-        if there are n piles given and k piles are to be merged. Then,
-        After the first merge : the length of the array will be n - (k-1).
-        After the second merge : the length of the array will be n - (k-1) - (k-1).
-        After the third merge : the length of the array will be n - (k-1) - (k-1) - (k-1)
-        and so on...
+if we do single merge of [5, 1, 2] => [8]
 
-        For eg:
-        [0 1 2 3 4] -> indexing
-        [3,5,1,2,6] -> array elements
-        length is 5
+[0,1,2] -> indexing
+[3,8,6] -> array elements
+length is 3 : which n - (k-1) = 5 - (k-1) = 5 - (3-1) = 5 - 2 = 3;
 
-        if we do single merge of [5, 1, 2] => [8]
+So, we know that after m merges the length must be 1, if we can merge piles
+So, we can say that Cost for single merge is k-1 and for m merges will be m*(k-1);
 
-        [0,1,2] -> indexing
-        [3,8,6] -> array elements
-        length is 3 : which n - (k-1) = 5 - (k-1) = 5 - (3-1) = 5 - 2 = 3;
+total_merges = m * (k-1);
 
-        So, we know that after m merges the length must be 1, if we can merge piles
-        So, we can say that Cost for single merge is k-1 and for m merges will be m*(k-1);
+Given in the question : A move consists of merging exactly k consecutive piles
+into one pile and in the end only single pile is left.,
 
-        total_merges = m * (k-1);
+So, we can say that:
+N - total_merges = 1
+N - m*(k-1) = 1;
+N - 1 = m*(k-1);
+m = (N-1) / (k-1)
 
-        Given in the question : A move consists of merging exactly k consecutive piles
-        into one pile and in the end only single pile is left.,
-
-        So, we can say that:
-        N - total_merges = 1
-        N - m*(k-1) = 1;
-        N - 1 = m*(k-1);
-        m = (N-1) / (k-1)
-
-        now, "m" must be positive number then only we can merge them
-        '''
+now, "m" must be positive number then only we can merge them
+'''
