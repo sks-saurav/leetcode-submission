69aa87abbff595866df25e337c7e12db3cd198ab
Saurav  Suman <60661830+sks-saurav@users.noreply.github.com>
2026-09-26 09:55:02 +0530

    Time: 19 ms (88.84%), Space: 19.3 MB (91.17%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -3,27 +3,24 @@ class Solution:
         '''  
         sum(P) - sum(N) = target
         sum(P) + sum(N) = totalSum
-       
         sum(P) = (target + totalSum) / 2
 
         Peduced to finding the number of ways to form subset with the sum = (target + totalSum) / 2
         '''
-        total_sum = sum(nums)
-        if abs(target) > total_sum or (total_sum + target) % 2 != 0:
+        totalSum = sum(nums)
+        if abs(target) > totalSum or (target+totalSum) % 2 != 0:
             return 0
-        
-        target_ss = (total_sum + target) // 2
-        n = len(nums)
-        dp = [0] * (target_ss + 1)
-        dp[0] = 1 
-       
-            
-        for i in range(n):
-            val = nums[i]
-            for j in range(target_ss, val-1, -1):
-                dp[j] = dp[j] + dp[j - val]
 
-        return dp[target_ss]
+        S = (target+totalSum)//2
+        dp = [0] * (S+1)
+        n = len(nums)
+        dp[0] = 1
+
+        for i in range(n):
+            for j in range(S, nums[i]-1, -1):
+                dp[j] += dp[j-nums[i]]
+        
+        return dp[S]
      
 
 
